1c29d44f89a52ccc245bb8dea5e9504a68372a11
Enoc Mena <enocschool@Enocs-iMac.local>
2021-07-13 13:11:09 -0400

    Initial commit

---
+++ src/test/java/com/ex26/PaymentCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.ex26;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateMonthsUntilPaidOff() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
